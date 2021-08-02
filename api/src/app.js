e380330a59b9d461bf5de67917ecebe8b4e1e4b6
Rocio Ayelen Juarez <juarez.rocio.a@gmail.com>
2021-08-02 20:35:52 -0300

    Base andandogit add .

---
+++ api/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const setHeaders =  require('./setHeaders')
+
+
+const server = express();
+
+server.name = 'API';
+
+//Seteamos los headers
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use(setHeaders) 
+
+//Seteamos las rutas
+server.use(cors());
+
+
+
+// Error catching endware.
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
---
+++ api/src/models/Producto.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+
+const Producto = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    abv: {
+
+    },
+    ibu: {
+
+    },
+    description: {
+
+    },
+    price: {
+        
+    }
+})
