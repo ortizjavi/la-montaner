17119d7db7a429dc2617afbab3965a27254e705d
gabiarrieta <gabriela.arrieta.187@gmail.com>
2021-08-03 20:09:50 -0300

    new product detail route

---
+++ api/src/routes/detailsProducts.js
@@ -0,0 +1,19 @@
+const Product = require("../models/Product");
+const { Router } = require("express");
+const detailProducts = Router();
+
+detailProducts.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    if (!id) return res.status(400).send({ error: "Id not found" });
+
+    let product = await Product.findById(id);
+    return res.json(product);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = detailProducts;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 const routerProducts = require('./addProducts')
+const detailsProducts = require('./detailsProducts');
 
 router.use('/admin', routerProducts)
+router.use('/products', detailsProducts)
 
 module.exports = router;
