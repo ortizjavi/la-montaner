04691f95f9979b08d08dc1e992097934259cba0b
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-08-03 16:58:20 -0500

    front

--- client/src/components/ProductCreation/ProductCreation.jsx
+++ client/src/components/ProductCreation/ProductCreation.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-export default function ProductCreation () {
+export default function ProductCreation() {
   return (
-    <div>Product Creation</div>
-  )
+    <div>
+      <form>
+        <input type="text" placeholder="nombre" />
+        <input type="text" placeholder="categoria" />
+        <input type="text" placeholder="img" />
+        <input type="number" placeholder="precio" />
+        <input type="number" placeholder="stock" />
+        <input type="number" placeholder="abv" />
+        <input type="number" placeholder="ibu" />
+        <textarea></textarea>
+        <input type="number" placeholder="volumen" />
+        <input type="text" placeholder="otros" />
+        <button type="submit">Crear</button>
+      </form>
+    </div>
+  );
 }
