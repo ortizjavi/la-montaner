3be1ba4e3f2e01e712a4d1f747325894c0603700
ortizjavi <ortizjavier160994@gmail.com>
2021-08-03 18:57:11 -0300

    fix(api/src/routes/index) add categories router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 const routerProducts = require('./addProducts')
+const routerCategories = require('./categories')
 
+router.use('/categories', routerCategories)
 router.use('/admin', routerProducts)
 
 module.exports = router;
