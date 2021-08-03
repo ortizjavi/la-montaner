d41313bf15dd51d441ed46b76c238379aa987138
William Cortes <williamcortesro@outlook.es>
2021-08-03 17:04:21 -0500

    Creación componernte Search y correccion en index.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Search from '../Search/Search';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <Search/>
+    </div>
   )
 }
