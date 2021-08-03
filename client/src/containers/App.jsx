1e19ae5bf9d5ba5f0a30c3d346eb0afed4b352c7
Nacho Hotz <juanhotz611@gmail.com>
2021-08-03 16:44:58 -0300

    feat(client/src/containers): done basic Route

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route } from 'react-router';
+import Home from '../components/Home/Home';
+import Landing from '../components/Landing/Landing';
+import ProductDetail from '../components/ProductDetail/ProductDetail';
+import ProductCreation from '../components/ProductCreation/ProductCreation';
 
 function App() {
   return (
-    <div className="App">
-      La montaner
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/home/creation" component={ProductCreation} />
+      <Route exact path="/home/:id" component={ProductDetail} />
     </div>
   );
 }
