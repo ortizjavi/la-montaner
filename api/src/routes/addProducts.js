90708d24d118a73c19e57843f217c64308cec548
Rocio Ayelen Juarez <juarez.rocio.a@gmail.com>
2021-08-03 19:00:51 -0300

    update y delete

--- api/src/routes/addProducts.js
+++ api/src/routes/addProducts.js
@@ -23,5 +23,47 @@ routerProducts.post('/add', async (req, res) => {
     }
 })
 
+routerProducts.put('/update/:id', async (req, res) => {
+    const productId = req.params.id;
+    const update = {
+        ...req.body}
+
+    try {
+        //const productoEncontrado = await Product.findById(productId);
+
+        const productoActualizado = await Product.findByIdAndUpdate(productId, update,{new:true})
+
+        res.json({
+            ok: true,
+            product: productoActualizado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            ok: false
+        })
+    }
+})
+
+routerProducts.delete('/delete/:id', async (req, res) => {
+    const productId = req.params.id;
+
+    try {
+
+        await Product.findByIdAndDelete(productId)
+
+        res.json({
+            ok: true
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            ok: false
+        })
+    }
+})
+
 module.exports = routerProducts;
 
