6dde6fa1d2a9b547a0539f77f027373c7ba8373e
Nacho Hotz <juanhotz611@gmail.com>
2021-08-03 16:08:49 -0300

    feat(client/src/reducer): done reducer structure

---
+++ client/src/reducer/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  allProducts: [],
+  productDetail: {},
+  allCategories: [],
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+export default rootReducer;
