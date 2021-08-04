cbd0b73ec70ce0ab774b7888b9df38a709c4b0b5
William Cortes <williamcortesro@outlook.es>
2021-08-04 01:02:23 -0500

    feat(client/src/components/searchBar + client/src/actions/types): permite buscar productos por el nombre

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { GET_PRODUCT_DETAIL, CREATE_CATEGORY } from './names';
+import { GET_PRODUCT_DETAIL, ALL_PRODUCTS, CREATE_CATEGORY } from './names';
 
 const SERVER = 'http://localhost:3001';
 const PRODUCTS_ENDPOINT = `${SERVER}/products`
+const ALL_PRODUCTS_ENDPOINT = `${SERVER}/search`
 const CATEGORIES_ENDPOINT = `${SERVER}/categories`
 
 export function getProductDetail(id) {
@@ -12,6 +13,16 @@ export function getProductDetail(id) {
         return dispatch({ type: GET_PRODUCT_DETAIL, payload: productDetail.data });
     } catch(e) {
       console.log(e);
+    }
+  }
+}
+export function getAllProducts() {
+  return async function(dispatch) {
+    try {
+        const response = await axios.get(`${ALL_PRODUCTS_ENDPOINT}`);
+        return dispatch({ type: ALL_PRODUCTS, payload: response.data });
+    } catch(e) {
+      console.log('actions/types/getAllProducts-Error:',e);
     }
   }
 }
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect }  from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import './SearchBar.css';
+import { getAllProducts } from '../../actions/types';
+import{ Link } from 'react-router-dom';
 
 const Search = ()=>{
 
@@ -12,6 +14,7 @@ const Search = ()=>{
 
     useEffect( () => {
        console.log('components/Search/state: ',state.product)
+       dispatch(getAllProducts())
       }, [state])
 
     const handleChange = (event) => {
@@ -20,21 +23,40 @@ const Search = ()=>{
       }
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert('me clickeaste')
         setState({ ...state, product: ""});
     }
-
+    console.log('components/searchBar: allProducts:', allProducts[0])
     return(
         <div>
-            <h2>Buscador</h2>
+            
             <form className="form-container" >
               
                     <label  >Buscar Producto: </label>
                     <input className='input_search' name='product' placeholder='Birras' value={state.product} onChange={(e) => handleChange(e)} />
-                    <button  type="button" onClick={(e) => handleSubmit(e)}>BUSCAR</button> 
-           
-
+                    <button  type="button" onClick={(e) => handleSubmit(e)}>Limpiar</button> 
             </form>
+            <section className='allproducts'>
+                {
+                    allProducts?.length && allProducts.filter(item => item.name.includes(state.product)).length ? 
+                    allProducts.filter(item => item.name.includes(state.product)).map( item =>
+                        <div className='product_container' key={item._id}>
+                            <Link className='link' to={`/home/${item?._id}`}>{item?.name}</Link>
+                            <br></br>
+                            <Link className='link' to={`/home/${item?._id}`}>
+                                <picture className='image_contain'>
+                                    {/* <img className="item_image" src={item?.image}  alt="Imagen de Birra" /> */}
+                                    <img className="item_image" src="https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg"  alt="Imagen de Birra" />
+                                </picture>
+                            </Link>
+                        
+                        
+                        </div>
+                    )
+                    :
+                        <h2> 😢 No hay productos que coincidan</h2>
+                     
+                }
+            </section>
         </div>
     )
 };
