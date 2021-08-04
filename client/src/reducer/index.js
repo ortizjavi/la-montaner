ce9772bf68f371e891c96290ebca316dc74a9121
baltasar <baltaguirre@gmail.com>
2021-08-03 23:37:55 -0300

    feat(client/src/):Route + reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,4 +1,4 @@
-import { GET_PRODUCT_DETAIL } from '../actions/names';
+import { GET_PRODUCT_DETAIL, CREATE_CATEGORY } from '../actions/names';
 
 const initialState = {
   allProducts: [],
@@ -9,6 +9,7 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   switch(action.type) {
     case GET_PRODUCT_DETAIL: return { ...state, productDetail: action.payload };
+    case CREATE_CATEGORY: return { ...state, allCategories: action.payload };
     default: return state;
   }
 }
