4bfee6dc10aeda5e8a4b8c4b153ad54bd960bd1d
Nacho Hotz <juanhotz611@gmail.com>
2021-08-04 14:41:05 -0300

    fix(client): frontend to back endpoints

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,7 @@
 export const SERVER = 'http://localhost:3001';
-export const CATEGORY_ENDPOINT = `${SERVER}/admin/category`;
+export const ADMIN_SERVER = `${SERVER}/admin`;
+export const CATEGORY_ENDPOINT = `${ADMIN_SERVER}/category`;
 export const GET_PRODUCTS_ENDPOINT = `${SERVER}`;
-export const CREATE_PRODUCT_ENDPOINT = `${SERVER}/product`;
-export const UPDATE_PRODUCT_ENDPOINT = `${SERVER}/product`;
-export const DELETE_PRODUCT_ENDPOINT = `${SERVER}/product`;
+export const CREATE_PRODUCT_ENDPOINT = `${ADMIN_SERVER}/product`;
+export const UPDATE_PRODUCT_ENDPOINT = `${ADMIN_SERVER}/product`;
+export const DELETE_PRODUCT_ENDPOINT = `${ADMIN_SERVER}/product`;
