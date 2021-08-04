78f338f5acd2a70b9cee6b6b1cdf20fc9ea65001
gabiarrieta <gabriela.arrieta.187@gmail.com>
2021-08-04 14:06:11 -0300

    feat(front/package.json):new react spinner dependencie

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const SERVER = 'http://localhost:3001';
+export const SERVER = 'http://localhost:3009';
 export const ALL_PRODUCTS_ENDPOINT = `${SERVER}/search`;
 export const PRODUCTS_ENDPOINT = `${SERVER}/products`;
 export const CATEGORIES_ENDPOINT = `${SERVER}/categories`;
