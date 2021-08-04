5cf5d54d7d0761811da59ea43e4a76d263a880df
Nacho Hotz <juanhotz611@gmail.com>
2021-08-04 00:22:32 -0300

    fix(api): index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app');
 const { conn } = require('./src/database');
 require('dotenv').config();
-const {dbConnection} = require('./src/database/index')
+const {dbConnection} = require('./src/database/index');
 
 dbConnection().then(() => {
   server.listen(process.env.PORT, () => {
   console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
-})})
+})});
 
