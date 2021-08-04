e2393e934d21e31c48f8db9b19da09be88adbc5f
Nacho Hotz <juanhotz611@gmail.com>
2021-08-04 19:32:53 -0300

    refactor(api/src): added semicolon

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(setHeaders) 
+server.use(setHeaders);
 
 //Seteamos las rutas
 server.use(cors());
