dad617c79b3e6b29cd2d9f378321dc2187252cb9
baltasar <baltaguirre@gmail.com>
2021-08-04 16:10:12 -0300

    feat(api/client/src/actions): getCategories action + name + type

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,4 +1,4 @@
-import { GET_PRODUCT_DETAIL, ALL_PRODUCTS, CREATE_CATEGORY } from '../actions/names';
+import { GET_PRODUCT_DETAIL, ALL_PRODUCTS, CREATE_CATEGORY, GET_CATEGORIES } from '../actions/names';
 
 const initialState = {
   allProducts: [],
@@ -10,7 +10,8 @@ const rootReducer = (state = initialState, action) => {
   switch(action.type) {
     case GET_PRODUCT_DETAIL: return { ...state, productDetail: action.payload };
     case ALL_PRODUCTS: return { ...state, allProducts: action.payload };
-    case CREATE_CATEGORY: return { ...state, allCategories: action.payload };
+    case CREATE_CATEGORY: return { ...state, allCategories: state.allCategories.concat(action.payload)};
+    case GET_CATEGORIES: return { ...state, allCategories: action.payload };
     default: return state;
   }
 }
