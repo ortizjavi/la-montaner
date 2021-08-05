aabe913b3764b66088673402fab778cf9f3218b7
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-08-05 15:08:43 -0500

    update button

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,35 +1,48 @@
 import "./App.css";
-import React, { useEffect }  from 'react';
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import Home from "../components/Home/Home";
 import Landing from "../components/Landing/Landing";
 import ProductDetail from "../components/ProductDetail/ProductDetail";
 import ProductCreation from "../components/ProductCreation/ProductCreation";
 import CategoryCreation from "../components/CategoryCreation/CategoryCreation";
-import { getCategories } from '../actions/types';
-
-
-
-
+import { getCategories } from "../actions/types";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+const theme = createTheme({
+  palette: {
+    primary: green,
+    secondary: lightGreen,
+  },
+});
 
 export default function App() {
   const dispatch = useDispatch();
-  useEffect( () => {
-   
-    dispatch(getCategories())
-   }, [])
-
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
 
   return (
     <div>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/home" component={Home} />
-      <Switch>
-        <Route exact path="/home/productCreation" component={ProductCreation} />
-        <Route exact path="/admin/categoryCreation" component={CategoryCreation} />
-        <Route exact path="/home/:id" component={ProductDetail} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
+        <Switch>
+          <Route
+            exact
+            path="/home/productCreation"
+            component={ProductCreation}
+          />
+          <Route
+            exact
+            path="/admin/categoryCreation"
+            component={CategoryCreation}
+          />
+          <Route exact path="/home/:id" component={ProductDetail} />
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
