b6943e709ab247654ac45b744c5bc5fe8cdcfc7b
William Cortes <williamcortesro@outlook.es>
2021-08-05 12:24:45 -0500

    feat(client/src/components/CategoryCreation/CategoryCreation.css): cambio en el heigth de la imagen

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect }  from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import './SearchBar.css';
-import { getAllProducts } from '../../actions/types';
+import { getAllProducts } from '../../actions/types/productActions.js';
 import{ Link } from 'react-router-dom';
 import Pagination from '../Pagination/Pagination';
 
