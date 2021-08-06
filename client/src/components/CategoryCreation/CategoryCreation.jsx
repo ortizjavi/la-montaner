aea9df4ef749b3e5b5bf0d83cbe87d3343e4e867
baltasar <baltaguirre@gmail.com>
2021-08-05 23:40:08 -0300

    feat(client/src/components/CategoryCreation): add button delete category + styles

--- client/src/components/CategoryCreation/CategoryCreation.jsx
+++ client/src/components/CategoryCreation/CategoryCreation.jsx
@@ -1,14 +1,16 @@
 import './CategoryCreation.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createCategory, deleteCategories } from '../../actions/types'
-import {fixedCategories} from '../../constants'
+import { createCategory, deleteCategories, getCategories } from '../../actions/types'
+import { fixedCategories } from '../../constants'
 
 
 export default function CategoryCreator() {
   const dispatch = useDispatch();
   const categories = useSelector(state => state.allCategories)
   const [state, setState] = useState("")
+
+
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -21,13 +23,14 @@ export default function CategoryCreator() {
     alert('Categoría creada con éxito')
     setState("");
   }
-  
+
   const handleClick = (id, name) => {
-   
     dispatch(deleteCategories(id))
     alert(`Categoría ${name.toUpperCase()} eliminada`)
-
   };
+
+
+
 
   return (
     <div className="category-create-container">
@@ -35,14 +38,20 @@ export default function CategoryCreator() {
         <input required className='input-create-category' name='product' placeholder='Categoría' value={state} onChange={(e) => handleChange(e)} />
         <button className="category-submit" type="submit">CREAR</button>
       </form>
+      <picture className='image_contain'>
+                                    <img className="item_image_form" src={'https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg'} placeholder="https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg" alt="Imagen de Birra" />
+                                    {/* <img className="item_image" src="https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg"  alt="Imagen de Birra" /> */}
+                                </picture>
       <ul className="category-list">
         <li className="category-list-title">Categorías Actuales</li>
-        <br/>
+        <br />
+
         {categories?.map((category, i) => (
           <li className="category-bullet" key={i}> {category.name.toUpperCase()}
-          {fixedCategories.includes(category.name.toLowerCase())? null :
-          <button className="delete-btn" onClick={() => handleClick(category._id, category.name)}>X</button>}
+            {fixedCategories.includes(category.name.toLowerCase()) ? null :
+              <button className="delete-btn" onClick={() => handleClick(category._id, category.name)}>X</button>}
           </li>
+
         ))}
       </ul>
     </div>
