b269ee70cc3709e501ffc810ada298424efe0968
baltasar <baltaguirre@gmail.com>
2021-08-06 14:10:31 -0300

    feat(client/src/components/categoryCreation): add alert repeated categories + styles corrected

--- client/src/components/CategoryCreation/CategoryCreation.jsx
+++ client/src/components/CategoryCreation/CategoryCreation.jsx
@@ -1,14 +1,13 @@
 import './CategoryCreation.css';
-import React, { useState, useEffect }  from 'react';
-import { useDispatch, useSelector} from 'react-redux';
-import { createCategory, deleteCategories, getCategories } from '../../actions/types/categoryActions';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createCategory, deleteCategories } from '../../actions/types/categoryActions';
 import { fixedCategories } from '../../utils/endpoints.js'
 
 export default function CategoryCreator() {
   const dispatch = useDispatch();
   const categories = useSelector(state => state.allCategories)
   const [state, setState] = useState("")
-
 
 
   const handleChange = (event) => {
@@ -18,17 +17,18 @@ export default function CategoryCreator() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (categories.filter(cat => cat.name === state).length) {
+      return alert('No puedes crear dos categorías con el mismo nombre!')
+    }
     dispatch(createCategory(state))
-    alert('Categoría creada con éxito')
+    alert('Categoría creada con éxito!')
     setState("");
   }
-  
+
   const handleClick = (category) => {
     dispatch(deleteCategories(category))
     alert(`Categoría ${category.name.toUpperCase()} eliminada`)
   };
-
-
 
 
   return (
@@ -37,18 +37,13 @@ export default function CategoryCreator() {
         <input required className='input-create-category' name='product' placeholder='Categoría' value={state} onChange={(e) => handleChange(e)} />
         <button className="category-submit" type="submit">CREAR</button>
       </form>
-      <picture className='image_contain'>
-                                    <img className="item_image_form" src={'https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg'} placeholder="https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg" alt="Imagen de Birra" />
-                                    {/* <img className="item_image" src="https://live.staticflickr.com/65535/51357138820_5d67c34fa6_m.jpg"  alt="Imagen de Birra" /> */}
-                                </picture>
       <ul className="category-list">
         <li className="category-list-title">Categorías Actuales</li>
         <br />
-
         {categories?.map((category, i) => (
           <li className="category-bullet" key={i}> {category.name.toUpperCase()}
-          {fixedCategories.includes(category.name.toLowerCase())? null :
-          <button className="delete-btn" onClick={() => handleClick(category)}>X</button>}
+            {fixedCategories.includes(category.name.toLowerCase()) ? null :
+              <button className="delete-btn" onClick={() => handleClick(category)}>X</button>}
           </li>
         ))}
       </ul>
