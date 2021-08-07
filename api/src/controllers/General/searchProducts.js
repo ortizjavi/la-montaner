e04c6b96953c5e20ae8f936eda5cce21b78078af
baltasar <baltaguirre@gmail.com>
2021-08-07 14:08:48 -0300

    feat(api/src/controllers/General/seachProducts):add function filter produts by category

--- api/src/controllers/General/searchProducts.js
+++ api/src/controllers/General/searchProducts.js
@@ -2,35 +2,60 @@ const Product = require('../../models/Product');
 
 module.exports = {
   searchProducts: async (req, res, next) => {
+    if(req.query.categories){
+      return filterProductsCategories(req,res,next);
+    }
+    
     let ProductsPerPage = 8;
     let pageNumber = req.query.pageNumber;
     let page = Math.max(0, pageNumber);
-    if (req.query.name){ //si se hace una búsqueda, se manda name por query
+    if (req.query.name) { //si se hace una búsqueda, se manda name por query
       try {
-          const name = req.query.name
-          const regex = new RegExp(`${name}+`, 'i') // i for case insensitive
-          const product = await Product.find({name: {$regex: regex}})
-                                        .limit(ProductsPerPage)
-                                        .skip(ProductsPerPage * page)
-                                        .sort({
-                                            name: 'asc'
-                                        })
-          const productLength = (await Product.find({name: {$regex: regex}})).length
-          return res.json([productLength, product]);
-        } catch (error) {
-          console.log(error);
-        }
+        const name = req.query.name
+        const regex = new RegExp(`${name}+`, 'i') // i for case insensitive
+        const product = await Product.find({ name: { $regex: regex } })
+          .limit(ProductsPerPage)
+          .skip(ProductsPerPage * page)
+          .sort({
+            name: 'asc'
+          })
+        const productLength = (await Product.find({ name: { $regex: regex } })).length
+        return res.json([productLength, product]);
+      } catch (error) {
+        console.log(error);
+      }
     } else { //si no se hace búsqueda se mandan todos los productos(dependiendo ProductsPerPage)
       const product = await Product.find()
-                                        .limit(ProductsPerPage)
-                                        .skip(ProductsPerPage * page)
-                                        .sort({
-                                            name: 'asc'
-                                        })
-      const productLength = await (await Product.find()).length                      
-          return res.json([productLength, product]);
+        .limit(ProductsPerPage)
+        .skip(ProductsPerPage * page)
+        .sort({
+          name: 'asc'
+        })
+      const productLength = await (await Product.find()).length
+      return res.json([productLength, product]);
     }
-      
+
   }
 }
+
+  const filterProductsCategories = async (req, res, next) => {
+    let ProductsPerPage = 8;
+    let pageNumber = req.query.pageNumber;
+    let page = Math.max(0, pageNumber);
+    
+      try {
+        const name = req.query.categories
+        const product = await Product.find({'categories.name': name})
+          .sort({
+            name: 'asc'
+          })
+          .skip(ProductsPerPage * page)
+          .limit(ProductsPerPage)
+        const productLength = product.length;
+        return res.json([productLength, product]);
+      } catch (error) {
+        console.log(error);
+      }
+    
+  }
 
