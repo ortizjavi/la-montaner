f494902c175f39ec9c621d972f8370a9898161f9
Nacho Hotz <juanhotz611@gmail.com>
2021-08-10 18:59:34 -0300

    fix(client/src/action): endpoints

--- client/src/actions/types/categoryActions.js
+++ client/src/actions/types/categoryActions.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 import * as actionTypes from '../names';
 
-import { ADMIN_CATEGORY_ENDPOINT } from '../../utils/endpoints';
+import { ADMIN_CATEGORY } from '../../utils/endpoints';
 
 export function createCategory(name) {
   return async function(dispatch) {
     try {
-        const response = await axios.post(`${ADMIN_CATEGORY_ENDPOINT}`, {name});
+        const response = await axios.post(`${ADMIN_CATEGORY}`, {name});
         return dispatch({ type: actionTypes.CREATE_CATEGORY, payload: response.data});
     } catch(e) {
       console.log(e);
@@ -18,7 +18,7 @@ export function createCategory(name) {
 export function getCategories() {
   return async function(dispatch) {
     try {
-        const response = await axios.get(`${ADMIN_CATEGORY_ENDPOINT}`);
+        const response = await axios.get(`${ADMIN_CATEGORY}`);
         return dispatch({ type: actionTypes.GET_CATEGORIES, payload: response.data });
     } catch(e) {
       
@@ -31,7 +31,7 @@ export function deleteCategories(category) {
     try {
         const response = await axios({
            method: 'delete',
-           url: `${ADMIN_CATEGORY_ENDPOINT}`,
+           url: `${ADMIN_CATEGORY}`,
            data: category
         });
         return dispatch({ type: actionTypes.DELETE_CATEGORY, payload: response.data._id });
