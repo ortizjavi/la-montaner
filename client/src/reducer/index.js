acc9023be35738475c1bfb8a035cad79f391cd6a
Nacho Hotz <juanhotz611@gmail.com>
2021-08-10 18:06:31 -0300

    refactor(client/src/utils): endpoints

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,21 +1,4 @@
-import {
-  GET_PRODUCT_DETAIL,
-  ALL_PRODUCTS,
-  CREATE_CATEGORY,
-  GET_CATEGORIES,
-  ADMIN_GET_PRODUCTS,
-  ADMIN_SELECT_PRODUCTS,
-  ADMIN_SELECT_DELETED_PRODUCTS,
-  DELETE_CATEGORY,
-  CURENT_PAGE,
-  SEARCH_STATE,
-  ADD_CART_PRODUCT,
-  DELETE_CART_PRODUCT,
-  ADMIN_CLEAR_SELECT,
-  ADMIN_SELECT_UPDATED_PRODUCTS,
-  FILTER_PRODUCTS_CATEGORY,
-  DELETE_CART_ALL,
-} from "../actions/names";
+import * as actionTypes from "../actions/names";
 
 const initialState = {
   allProducts: [],
@@ -31,18 +14,18 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_PRODUCT_DETAIL: return { ...state, productDetail: action.payload };
-    case ALL_PRODUCTS: return { ...state, allProducts: action.payload };
-    case CREATE_CATEGORY: return { ...state, allCategories: state.allCategories.concat(action.payload) };
-    case DELETE_CATEGORY: return { ...state, allCategories: state.allCategories.filter(cat => cat._id !== action.payload) };
-    case GET_CATEGORIES: return { ...state, allCategories: action.payload };
-    case ADMIN_GET_PRODUCTS: return { ...state, adminProducts: action.payload };
-    case ADMIN_SELECT_PRODUCTS: return { ...state, selectedAdminProducts: action.payload };
-    case ADMIN_SELECT_DELETED_PRODUCTS: return { ...state, selectedAdminProducts: [] };
-    case CURENT_PAGE: return { ...state, currentPage: action.payload };
-    case SEARCH_STATE: return { ...state, searchProdustsState: action.payload };
-    case FILTER_PRODUCTS_CATEGORY: return { ...state, allProducts: action.payload };
-    case ADD_CART_PRODUCT: 
+    case actionTypes.GET_PRODUCT_DETAIL: return { ...state, productDetail: action.payload };
+    case actionTypes.ALL_PRODUCTS: return { ...state, allProducts: action.payload };
+    case actionTypes.CREATE_CATEGORY: return { ...state, allCategories: state.allCategories.concat(action.payload) };
+    case actionTypes.DELETE_CATEGORY: return { ...state, allCategories: state.allCategories.filter(cat => cat._id !== action.payload) };
+    case actionTypes.GET_CATEGORIES: return { ...state, allCategories: action.payload };
+    case actionTypes.ADMIN_GET_PRODUCTS: return { ...state, adminProducts: action.payload };
+    case actionTypes.ADMIN_SELECT_PRODUCTS: return { ...state, selectedAdminProducts: action.payload };
+    case actionTypes.ADMIN_SELECT_DELETED_PRODUCTS: return { ...state, selectedAdminProducts: [] };
+    case actionTypes.CURENT_PAGE: return { ...state, currentPage: action.payload };
+    case actionTypes.SEARCH_STATE: return { ...state, searchProdustsState: action.payload };
+    case actionTypes.FILTER_PRODUCTS_CATEGORY: return { ...state, allProducts: action.payload };
+    case actionTypes.ADD_CART_PRODUCT: 
       const product = action.payload;
       const existItem = state.cartProducts.find((p) => p.id === product.id);
 
@@ -51,8 +34,8 @@ const rootReducer = (state = initialState, action) => {
       } else {
         return { ...state, cartProducts: [...state.cartProducts, product] };
       }
-    case DELETE_CART_PRODUCT: return { ...state, cartProducts: state.cartProducts.filter((product) => product.id !== action.paylaod) };
-    case DELETE_CART_ALL: return { ...state, cartProducts: [] };
+    case actionTypes.DELETE_CART_PRODUCT: return { ...state, cartProducts: state.cartProducts.filter((product) => product.id !== action.paylaod) };
+    case actionTypes.DELETE_CART_ALL: return { ...state, cartProducts: [] };
     default: return state;
   }
 };
