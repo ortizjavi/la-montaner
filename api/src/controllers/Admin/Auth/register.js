29f2e0cbcd0d6e127f5122cd95350fbd188e2847
ortizjavi <ortizjavier160994@gmail.com>
2021-08-10 14:39:27 -0300

    feat(back/controllers/Admin/Auth): login & register endpoints

--- api/src/controllers/Admin/Auth/register.js
+++ api/src/controllers/Admin/Auth/register.js
@@ -1,3 +1,19 @@
-module.exports = () => {
+require('dotenv').config();
+const User = require('../models/Users/User');
+const { SALT_ROUNDS, TOKEN_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+	const { email, password } = req.body;
+	User.hashPassword(password, Number(SALT_ROUNDS)).then((hashed) => {
+		User.create({
+			...req.body,
+			password : hashed,
+			role: undefined
+		}).then(() => {
+			res.send('Ok');
+		}).catch((err) => {
+			res.status(400).send('User already exists')
+		})
+	})
 	
 }
