555e1e6deacb81e4a2d683f9003e4ecfc4f14567
ortizjavi <ortizjavier160994@gmail.com>
2021-08-11 12:20:46 -0300

    feat(back/utils/signJWT): util function to sign jwt's

---
+++ api/src/utils/signJWT.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const { TOKEN_SECRET } = process.env;
+
+module.exports = (_id) => {
+	return new Promise((resolve, reject) => {
+		jwt.sign({ //create jwt token
+		_id
+		}, TOKEN_SECRET
+		, { expiresIn: '36h' }
+		, function(err, token){
+			console.error(err);
+			if (err) return reject(err);
+			resolve(token);
+		});
+	})
+}
