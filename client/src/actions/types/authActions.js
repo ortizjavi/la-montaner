442c1ebfdc041560ef1fc16e48fdd43318e1d862
ortizjavi <ortizjavier160994@gmail.com>
2021-08-10 23:21:54 -0300

    feat(front/actions/types): google action to login

--- client/src/actions/types/authActions.js
+++ client/src/actions/types/authActions.js
@@ -1,3 +1,14 @@
 import axios from 'axios';
+import * as actionTypes from '../names';
+import * as endpoints from '../../utils/endpoints';
 
+export async function loginWithGoogle(token) {
+    try {
+        const response = await axios.post(`${endpoints.AUTH_LOGIN}`, 
+        											{ google: token });
+        // return dispatch({ type: actionTypes.LOGIN_USER, payload: response.data});
+    } catch(e) {
+      console.log(e);
+    }
+}
 
