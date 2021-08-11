d07638588327368fbcedbe56adf90b154b8bfab0
ortizjavi <ortizjavier160994@gmail.com>
2021-08-11 12:21:55 -0300

    feat(back/controllers/Auth): new file externalLogin to handle google and FB

---
+++ api/src/controllers/Admin/Auth/externalLogin.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const User = require('../../../models/Users/User');
+const verifyGoogle = require('../../../utils/validateGoogleToken');
+const verifyFB = require('../../../utils/validateFBToken');
+const signJWT = require('../../../utils/signJWT');
+const crypto = require('crypto');
+
+
+const mapFacebookProperties = (obj) => {
+	return {
+		...obj,
+		given_name: obj.first_name,
+		family_name: obj.last_name,
+		picture: obj.picture.data.url,
+		email_verified: false
+	}
+}
+
+const googleLogin = (googleToken) => {
+	return verifyGoogle(googleToken);	
+}
+
+const facebookLogin = (facebookToken) => {
+	return verifyFB(facebookToken)
+			.then(mapFacebookProperties)
+}
+
+module.exports = (req, res, next) => {
+	let token = '', login = null;
+	if (req.body.google){
+		token = req.body.google;
+		login = googleLogin;
+	} else {
+		token = req.body.facebook;
+		login = facebookLogin;
+	}
+	
+	login(token).then((payload) => {
+		return User.findOne({ email: payload.email })
+		.then((user) => {
+			if (user){
+				return user;
+			}
+
+			return User.create({ 
+				email : payload.email,
+				verified : payload.email_verified,
+				given_name: payload.given_name,
+				family_name: payload.family_name,
+				picture: payload.picture,
+				name: payload.name,
+				password: crypto.randomBytes(64).toString('hex')
+			})
+		})
+	})
+	.then((user) => {
+		const { password, _id, ...userProps } = user._doc;
+		signJWT(_id).then(() => {
+			res.status(200).json({ ...userProps, _id, token });
+		}).catch((err) => {
+			console.error(err);
+			return res.sendStatus(500);
+		})
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(400).json({ message: 'Invalid user or password' });
+	})
+}
+
+
--- api/src/controllers/Admin/Auth/login.js
+++ api/src/controllers/Admin/Auth/login.js
@@ -1,80 +1,14 @@
 require('dotenv').config();
 const User = require('../../../models/Users/User');
-const jwt = require('jsonwebtoken');
-const { TOKEN_SECRET, FB_ACCESS_TOKEN } = process.env;
-const verify = require('../../../utils/validateGoogleToken');
-const crypto = require('crypto');
-const axios = require('axios');
+const externalLogin = require('./externalLogin');
+const signJWT = require('../../../utils/signJWT');
 
-const googleLogin = (req, res, next) => {
-	verify(req.body.google)
-	.then((payload) => {
-		return User.findOne({ email: payload.email })
-		.then((user) => {
-			if (user){
-				return user;
-			}
 
-			return User.create({ 
-				email : payload.email,
-				verified : payload.email_verified,
-				given_name: payload.given_name,
-				family_name: payload.family_name,
-				picture: payload.picture,
-				name: payload.name,
-				password: crypto.randomBytes(64).toString('hex')
-			})
-		})
-	})
-	.then((user) => {
-		const { password, _id, ...userProps } = user._doc;
-		signToken(_id, function(err, token){
-			console.error(err);
-			if (err) return res.sendStatus(500);
-			res.status(200).json({ ...userProps, _id, token })
-		});
-	})
-	.catch(err => {
-		console.error(err);
-		res.status(400).send('Invalid user or password');
-	})
-}
-
-const facebookLogin = async (req, res, next) => {
-	try {
-		console.log(req.body.facebook);
-	  const { data } = await axios({
-	    url: 'https://graph.facebook.com/me',
-	    method: 'get',
-	    params: {
-	      fields: ['id', 'email', 'first_name', 'last_name', 'picture', 'name'].join(','),
-	      access_token: req.body.facebook,
-	    },
-	  });
-	  console.log(data); // { id, email, first_name, last_name }
-	  res.json(data);
-	} catch (err){
-		console.error(err);
-		res.json('No');
-	}
-}
-
-const signToken = (_id, cb) => {
-	jwt.sign({ //create jwt token
-		_id
-	}, TOKEN_SECRET
-	, { expiresIn: '36h' }
-	, cb)
-}
 
 
 module.exports = (req, res, next) => {
-	if (req.body.google){
-		return googleLogin(req, res, next);
-	}
-
-	if (req.body.facebook){
-		return facebookLogin(req, res, next);
+	if (req.body.google || req.body.facebook){
+		return externalLogin(req, res, next);
 	}
 
 	const email = req.body.email;
@@ -83,15 +17,15 @@ module.exports = (req, res, next) => {
 		const { password, _id, ...userProps } = user._doc;
 
 		User.comparePassword(passwordSent, password).then((isEqual) => {
-
 			if (!isEqual) //pass invalid
 				return res.status(400).send('Invalid user or password');
 			// create JWT
-			signToken(_id, function(err, token){
+			signJWT(_id).then(() => {
+				res.status(200).json({ ...userProps, _id, token });
+			}).catch((err) => {
 				console.error(err);
-				if (err) return res.sendStatus(500);
-				res.status(200).json({ ...userProps, _id, token })
-			});
+				return res.sendStatus(500);
+			})
 		}).catch(err => {
 			console.error(err);
 			res.status(400).send('Invalid user or password')
