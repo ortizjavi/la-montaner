a0a8a0c8dd36d28d51ecc297e511360741791ab3
Nacho Hotz <juanhotz611@gmail.com>
2021-08-11 13:44:36 -0300

    feat(client/src/reducer): add DELETE_CART_ALL

--- client/src/reducer/cartReducers.js
+++ client/src/reducer/cartReducers.js
@@ -7,31 +7,18 @@ const CART_INITIAL_STATE = {
 const cartReducer = (state = CART_INITIAL_STATE, action) => {
     switch (action.type) {
       case actionTypes.ADD_CART_PRODUCT:
-        const item = action.payload;
-  
-        const existItem = state.cartItems.find((x) => x.id === item.id);
+        const newProduct = action.payload;
+        const existItem = state.cartItems.find((product) => product.id === newProduct.id);
   
         if (existItem) {
-          return {
-            ...state,
-            cartItems: state.cartItems.map((x) =>
-              x.id === existItem.id ? item : x
-            ),
-          };
+          return { ...state, cartItems: state.cartItems.map((product) => product.id === existItem.id ? newProduct : product) };
         } else {
-          return {
-            ...state,
-            cartItems: state.cartItems.concat(item),
-          };
+          return { ...state, cartItems: state.cartItems.concat(newProduct) };
         }
-      case actionTypes.DELETE_CART_PRODUCT:
-        return {
-          ...state,
-          cartItems: state.cartItems.filter((x) => x.id !== action.payload),
-        };
-      default:
-        return state;
+      case actionTypes.DELETE_CART_PRODUCT: return { ...state, cartItems: state.cartItems.filter((product) => product.id !== action.payload) };
+      case actionTypes.DELETE_CART_ALL: return { ...state, cartItems: [] };
+      default: return state;
     }
   };
 
-  export default cartReducer;
+export default cartReducer;
