28d67cc4ab0e74f55dcf32baf714a09050c97e17
Nacho Hotz <juanhotz611@gmail.com>
2021-08-10 23:20:01 -0300

    fix(client/src/store): add localStorage to redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducer/index';
+
+const middleware = [thunk];
+
+const cartItemsInLocalStorage = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+
+const INITIAL_STATE = { 
+  cartProducts: cartItemsInLocalStorage,
+}
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk),
-   window.__REDUX_DEVTOOLS_EXTENSION__
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : f => f)
+  INITIAL_STATE,
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 export default store;
-
