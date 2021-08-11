372e13d2e1699aca0d72340d784d2dbf38ed6f76
ortizjavi <ortizjavier160994@gmail.com>
2021-08-11 16:17:25 -0300

    feat(front/actions/types/authActions): loadUserSession to get user and token from sessionStorage

--- client/src/actions/types/authActions.js
+++ client/src/actions/types/authActions.js
@@ -5,16 +5,10 @@ import * as endpoints from '../../utils/endpoints';
 export function loginWithGoogle(token) {
   return async function(dispatch){  
     try {
-        const response = await axios({
-          method : 'post', 
-          url : `${endpoints.AUTH_LOGIN}`,
-          data: { google: token },
-          headers: {
-            Authorization: 'Bearer' + token
-          }										
-    })
+        const response = await axios.post(`${endpoints.AUTH_LOGIN}`, 
+        											{ google: token });
         console.log(response);
-         return dispatch({ type: actionTypes.LOGIN_USER_GOOGLE, payload: response.data});
+         return dispatch({ type: actionTypes.LOGIN_USER, payload: response.data});
     } catch(e) {
       console.log(e);
     }
@@ -24,19 +18,23 @@ export function loginWithGoogle(token) {
 export function loginWithFacebook(token) {
   return async function(dispatch){  
     try {
-        const response = await axios({
-          method : 'post', 
-          url : `${endpoints.AUTH_LOGIN}`,
-          data: { facebook: token },
-          headers: {
-            Authorization: 'Bearer' + token
-          }										
-    })
+        const response = await axios.post(`${endpoints.AUTH_LOGIN}`, 
+        											{ facebook: token });
         console.log(response);
-    	return dispatch({ type: actionTypes.LOGIN_USER_FACEBOOK, payload: response.data});
+    	return dispatch({ type: actionTypes.LOGIN_USER, payload: response.data});
     } catch(e) {
       console.log(e);
     }
+  }
+}
+
+export function loadUserSession() {
+  return async function(dispatch){  
+    let session = JSON.parse(sessionStorage.getItem('userSession'));
+    if (session.user){
+      dispatch({ type: actionTypes.LOGIN_USER, payload: session.user });
+    }
+    return
   }
 }
 
