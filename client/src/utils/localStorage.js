add0579e6b20a8cef0d6042c2275c5578d670ce1
Nacho Hotz <juanhotz611@gmail.com>
2021-08-13 14:52:18 -0300

    feat(client/src/redux): wishlistItemsInLocalStorage

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,8 +1,8 @@
 const cart = localStorage.getItem('cart');
 export const cartItemsInLocalStorage = cart ? JSON.parse(cart) : [];
 
-let sessionStorage = localStorage.getItem('session');
-export const session = sessionStorage ? JSON.parse(sessionStorage) : { token: '' };
-
 const wishlist = localStorage.getItem('wishlist');
 export const wishlistItemsInLocalStorage = wishlist ? JSON.parse(wishlist) : [];
+
+let sessionStorage = localStorage.getItem('session');
+export const session = sessionStorage ? JSON.parse(sessionStorage) : { token: '' };
