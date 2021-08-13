dbe810a17d10f822015a801711b7ca9a62c0e91b
Nacho Hotz <juanhotz611@gmail.com>
2021-08-13 18:48:33 -0300

    feat(client/src/redux): update reducer

--- client/src/redux/reducer/wishlistReducer.js
+++ client/src/redux/reducer/wishlistReducer.js
@@ -6,7 +6,15 @@ const WISHLIST_INITIAL_STATE = {
 
 const wishlistReducer = (state = WISHLIST_INITIAL_STATE, action) => {
   switch(action.type) {
-    case actionTypes.ADD_FAV_PRODUCT: return { ...state, wishlistItems: state.wishlistItems.concat(action.payload) };
+    case actionTypes.ADD_FAV_PRODUCT: 
+      const newFav = action.payload;
+      const existFav = state.wishlistItems.find((product) => product.id === newFav.id);
+
+      if (existFav) {
+        return { ...state, wishlistItems: state.wishlistItems.map((product) => product.id === existFav.id ? newFav : product) };
+      } else {
+        return { ...state, wishlistItems: state.wishlistItems.concat(newFav) };
+      }
     case actionTypes.DELETE_FAV_PRODUCT: return { ...state, wishlistItems: state.wishlistItems.filter((fav) => fav.id !== action.payload) }
     default: return state
   }
