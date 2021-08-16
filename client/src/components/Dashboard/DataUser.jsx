c404be19ac26e015f73d1940d33ab5d734918606
William Cortes <williamcortesro@outlook.es>
2021-08-16 15:50:01 -0500

    feat(client/src/components/ShowProducts/ShowProducts.css+Dashboard): cambio estilo de boton y en la tabla dashboard usuario

--- client/src/components/Dashboard/DataUser.jsx
+++ client/src/components/Dashboard/DataUser.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 import {NavLink} from 'react-router-dom';
 import { BsHeart } from 'react-icons/bs';
 import { HiOutlinePhotograph  } from 'react-icons/hi';
@@ -7,20 +8,28 @@ import { IoIosNotificationsOutline  } from 'react-icons/io';
 import './DataUser.css';
 
 const DataUser = () =>{
+    const user = useSelector((state) => state.session.user);
+
+    console.log('datauser/user:',user.name)
+
     return(
         <div className='dataUser-container'>
-            <HiOutlinePhotograph style={{fontSize: 30, color:"grey"}}/>
-            Hola, {'Nombre'}
+           {user && user.picture ? 
+                <img className='du-image' src={user.picture} alt="imagen de usuario" width="80" height="80" ></img> 
+            : 
+            <HiOutlinePhotograph style={{fontSize: 30, color:"grey"}}/>}
+            <h4> Hola, {user.name.split(' ')[0]}</h4>
+
             <div className='dataUser-section'>
-                <NavLink to='/dashboard'>
+                <NavLink className='du-link' to='/wishlist'>
                     <BsHeart style={{ fontSize: 30, color:"grey" }}/>
                     <h5>Lista De Deseos</h5>
-                </NavLink>
-                <NavLink to='/dashboard'>
+                </NavLink >
+                <NavLink className='du-link' to='/dashboard'>
                     <FaComments style={{fontSize: 30, color:"grey"}}/>
                     <h5> Comentarios</h5>
                 </NavLink>
-                <NavLink to='/dashboard'>
+                <NavLink className='du-link' to='/dashboard'>
                     <IoIosNotificationsOutline style={{fontSize: 30, color:"grey"}}/>
                     <h5>Notificaciones</h5>
                 </NavLink>
