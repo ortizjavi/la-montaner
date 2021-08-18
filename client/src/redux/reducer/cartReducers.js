32e3072c718edac20e3a9c3fc7df6cabf5ea222d
Nacho Hotz <juanhotz611@gmail.com>
2021-08-17 23:06:29 -0300

    fix(client/src/components): linter

--- client/src/redux/reducer/cartReducers.js
+++ client/src/redux/reducer/cartReducers.js
@@ -9,32 +9,17 @@ const cartReducer = (state = CART_INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.ADD_CART_PRODUCT:
       const newProduct = action.payload;
-      const existItem = state.cartItems.find(
-        (product) => product.id === newProduct.id
-      );
+      const existItem = state.cartItems.find( (product) => product.id === newProduct.id);
 
       if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((product) =>
-            product.id === existItem.id ? newProduct : product
-          ),
-        };
+        return { ...state, cartItems: state.cartItems.map((product) => product.id === existItem.id ? newProduct : product) };
       } else {
         return { ...state, cartItems: state.cartItems.concat(newProduct) };
       }
-    case actionTypes.DELETE_CART_PRODUCT:
-      return {
-        ...state,
-        cartItems: state.cartItems.filter(
-          (product) => product.id !== action.payload
-        ),
-      };
-    case actionTypes.DELETE_CART_ALL:
-      return { ...state, cartItems: [] };
+    case actionTypes.DELETE_CART_PRODUCT: return { ...state, cartItems: state.cartItems.filter( (product) => product.id !== action.payload) };
+    case actionTypes.DELETE_CART_ALL: return { ...state, cartItems: [] };
     case actionTypes.ADD_CART_SUB_TOTAL: return { ...state, cartSubtotal: action.payload };
-    default:
-      return state;
+    default: return state;
   }
 };
 
