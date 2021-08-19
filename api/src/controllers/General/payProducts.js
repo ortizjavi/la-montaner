ff5779e2da672c5adcebabdf36d2d8e1a53a72fe
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-08-19 17:04:45 -0500

    errors

--- api/src/controllers/General/payProducts.js
+++ api/src/controllers/General/payProducts.js
@@ -6,15 +6,11 @@ mercadopago.configure({
     "TEST-7665548571999255-081017-8db4487dcda48e5cc676f0d587b05af6-13063487",
 });
 //Tabla de ordenes de compra
-const Order = require('../../models/Orders');
 
 module.exports = {
   payProducts: async (req, res) => {
-
-    const cartItems = req.body.compra
+    const cartItems = req.body.compra;
     try {
-
-
       let preference = {
         back_urls: {
           success: "http://localhost:3000/home/pay/success",
@@ -22,17 +18,15 @@ module.exports = {
           pending: "http://localhost:3000/home/pay/pending",
         },
         auto_return: "approved",
-        items: []
-
-        
+        items: [],
       };
 
-      for(let i=0; i<cartItems.length; i++){
+      for (let i = 0; i < cartItems.length; i++) {
         let title = cartItems[i].name;
         let unit_price = cartItems[i].price;
         let cantidad = parseInt(cartItems[i].stockSelected);
         let quantity = cantidad;
-        preference.items.push({title, unit_price,quantity})
+        preference.items.push({ title, unit_price, quantity });
       }
       const wii = await mercadopago.preferences.create(preference);
       res.json(wii);
@@ -40,29 +34,4 @@ module.exports = {
       console.log(err);
     }
   },
-
-  /* getPayProducts: async(req,res) => {
-    const product =req.params;
-    try {
-      const compra = await Order.findById(id);
-
-      const carrito = await Cart.findById(compra.cart);
-
-      let compra2= [];
-      for(let i=0; i<carrito.producto.length; i++){
-        let title = carrito.producto[i].name;
-        let unit_price = carrito.producto[i].price;
-        let quantity = carrito.quantity[i];
-        compra2.push({title, unit_price,quantity})
-      }
-      res.json({
-        compra2
-      });
-    } catch (error) {
-      console.log(error)
-      res.json({
-        ok: false
-      })
-    }
-  } */
 };
