ff1716efc3122375b7d2474895133aac9b121ed7
baltasar <baltaguirre@gmail.com>
2021-08-19 12:29:51 -0300

    (src/components/Navbar/NavBar.jsx):fix addEventListener add check on header.classList.toggle

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -64,7 +64,7 @@ function NavBar(props, {history}) {
     
       window.addEventListener('scroll', function () {
         var header = document.querySelector('header');
-        header.classList.toggle('sticky', window.scrollY > 0)
+        header?.classList.toggle('sticky', window.scrollY > 0)
       })
 
  return (
