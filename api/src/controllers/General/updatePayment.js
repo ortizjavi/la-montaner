0f7bfe1e54514c7ac54804141f64669d6d21fd90
ortizjavi <ortizjavier160994@gmail.com>
2021-08-20 13:24:34 -0300

    feat(api/src/html): email template + email sent when updating payment status

--- api/src/controllers/General/updatePayment.js
+++ api/src/controllers/General/updatePayment.js
@@ -1,4 +1,5 @@
 const Order = require("../../models/Orders");
+const { processingOrder } = require('../../utils/sendMail');
 
 module.exports = (req, res, next) => {
 	const { 
@@ -6,12 +7,34 @@ module.exports = (req, res, next) => {
 	 payment_method, 
 	 status 
 	} = req.body;
-	const orderStatus = status !== 'null' ? 'Procesando' : 'Cancelada';
+	const statusNotNull = status !== 'null';
+	const orderStatus =  statusNotNull ? 'Procesando' : 'Cancelada';
 
 	Order.findOneAndUpdate({ mp_preference },
 		{ payment_status: status, status: orderStatus, payment_method },
 		{ new : true},
 		(err, doc) => {
+			let total = doc.cart.reduce((acum, product) => {
+				return acum += product.price;
+			},0)
+			if (statusNotNull){
+				processingOrder(
+					req.user.email, 
+					req.user.name,
+					{
+						total,
+						method: doc.payment_method
+					},
+					{
+						delivery: doc.address.length,
+						address: doc.address
+					}
+				).then(() => {
+					console.log('email sent');
+				}).catch((err) => {
+					console.error(err);	
+				})
+			}
 			res.json({ ok: true, order: doc });
 		}
 	)
