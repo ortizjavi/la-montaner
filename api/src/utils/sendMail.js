07ad9ae68b374a1ec7631091e6d53e2b0cbb053a
ortizjavi <ortizjavier160994@gmail.com>
2021-08-23 20:26:35 -0300

    feat(back/utils/sendEmail & back/html/emailTemplate): new order complete template

--- api/src/utils/sendMail.js
+++ api/src/utils/sendMail.js
@@ -13,6 +13,16 @@ const transport = nodemailer.createTransport({
 	},
 })
 
+let html_template = '', processTemplate = '', completedTemplate = '';
+fs.readFile(path.resolve(__dirname, '../html/emailTemplate.html'), (err, data) => {
+	html_template = data.toString();
+	processTemplate = html_template.replace('<!-- ORDER_SUMMARY', '')
+											.replace('ORDER_SUMMARY -->', '');
+	completedTemplate = html_template.replace('<!-- COMPLETED_ORDER', '')
+											.replace('COMPLETED_ORDER -->', '');
+});
+
+
 const sendEmail = {};
 
 sendEmail.sendFormEmail = (email,name) => {
@@ -28,43 +38,71 @@ sendEmail.sendFormEmail = (email,name) => {
 
 
 sendEmail.processingOrder = (email, name, payment, shipping) => {
-	return fs.readFile(path.resolve(__dirname, '../html/emailTemplate.html'), (err, data) => {
-		data = data.toString();
-		data = data.replace('{title}', `Hola ${name}, tu orden fue confirmada!`);
-		data = data.replace('{paymentTitle}', `Pagaste $ ${payment.total}`);
-		let shippingTitle, shippingAddress;
-		if(shipping.delivery){
-			shippingTitle = 'Envío a domicilio';
-			shippingAddress = shipping.address;
-		} else {
-			shippingTitle = 'Retiro por local';
-			shippingAddress = 'Dirección de Chicha';
-		}
-		data = data.replace('{paymentInfo}', `con ${payment.method}`);
-		data = data.replace('{shippingTitle}', shippingTitle);
-		data = data.replace('{shippingInfo}', `${shippingAddress}`);
-		
-		return transport.sendMail({
-			from: `La Montañes <${process.env.MAIL_USER}>`,
-			to: email,
-			subject: `Gracias por tu compra!`,
-			html: data
-		})
+	let data = html_template;
+	data = data.replace('{title}', `Hola ${name}, tu orden fue confirmada!`);
+	let paymentTitle;
+	if (payment.method === 'Efectivo'){
+		paymentTitle = 'A pagar'
+	} else {
+		paymentTitle = 'Pagaste'
+	}
+	data = data.replace('{paymentTitle}', `${paymentTitle} $ ${payment.total}`);
+	let shippingTitle, shippingAddress;
+	if(shipping.delivery){
+		shippingTitle = 'Envío a domicilio';
+		shippingAddress = shipping.address;
+	} else {
+		shippingTitle = 'Retiro por local';
+		shippingAddress = 'Dirección de Chicha';
+	}
+	data = data.replace('{paymentInfo}', `con ${payment.method}`);
+	data = data.replace('{shippingTitle}', `${shippingTitle}`);
+	data = data.replace('{shippingInfo}', `${shippingAddress}`);
+	
+	return transport.sendMail({
+		from: `La Montañes <${process.env.MAIL_USER}>`,
+		to: email,
+		subject: `Gracias por tu compra!`,
+		html: data
 	})
 }
 
+sendEmail.completedOrder = (email, name, shipping) => {
+	let data = completedTemplate;
+	data = data.replace(/{title}/g, `Hola ${name}, tu orden fue actualizada!`);
+
+	let shippingTitle, shippingAddress;
+	if(shipping.delivery){
+		data = data.replace('{deliveryMssg}', 'Tu birra está en camino 🍺');
+		shippingTitle = 'Dirección de envío';
+		shippingAddress = shipping.address;
+	} else {
+		data = data.replace('{deliveryMssg}', 'Tu birra te espera 🍺');
+		shippingTitle = 'Retiro por local';
+		shippingAddress = 'Dirección de Chicha';
+	}
+	data = data.replace(/{shippingTitle}/g, `${shippingTitle}`);
+	data = data.replace(/{shippingInfo}/g, `${shippingAddress}`);
+
+	return transport.sendMail({
+		from: `La Montañes <${process.env.MAIL_USER}>`,
+		to: email,
+		subject: `Tu orden está lista!`,
+		html: data
+	})
+}
+
+
 sendEmail.passRecoveryEmail = (email,name, newPass) => {
-	return fs.readFile(path.resolve(__dirname, '../html/passRecoveryTemplate.html'), (err, data) => {
-		data = data.toString();
-		data = data.replace('{title}', `Hola ${name}`);
-		data = data.replace('{shippingTitle}', newPass);
+	let data = html_template;
+	data = data.replace('{title}', `Hola ${name}`);
+	data = data.replace('{shippingTitle}', newPass);
 	return transport.sendMail({
 		from: `La Montañes <${process.env.MAIL_USER}>`,
 		to: email,
 		subject: "Gracias por contactarte con La Montañes!",
 		html: data
 	});	
-})
 }
 
 module.exports = sendEmail;
