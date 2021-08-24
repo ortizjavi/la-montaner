a75c817fd9a48a36f4c2f64d8fbc8260fe61d2ba
Nacho Hotz <juanhotz611@gmail.com>
2021-08-23 23:00:43 -0300

    feat(client/src/components): FindUs component

--- client/src/components/FindUs/FindUs.jsx
+++ client/src/components/FindUs/FindUs.jsx
@@ -2,76 +2,77 @@ import { React } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { FaWhatsapp } from "react-icons/fa";
 import {WHATSAPP_LINK} from '../../utils/constants';
+import './FindUs.css';
 //import { useForm } from 'react-hook-form';
-import swal from "sweetalert";
+//import swal from "sweetalert";
 
-const FindUs = () => {
-    const mapStyle = { height: '400px', width: '600px', margin: '0 auto'};
-    const location = { lat: -30.856862725386204, lng: -64.51534707428485};
-    // const { register, errors, handleSubmit } = useForm();
+import { GOOGLE_API_KEY } from '../../utils/constants.js';
 
-	// const handleForm = (data, e) => {
-	// 	console.log('datos enviados')
-	// 	e.target.reset();
-	// 	swal('Su consulta ha sido enviada', 
-    //     {
-	// 		icon: "success",
-	// 	});
-	// };
+export default function FindUs() {
+  const mapStyle = { height: '400px', width: '600px', margin: '0 auto'};
+  const location = { lat: -30.856862725386204, lng: -64.51534707428485};
+  // const { register, errors, handleSubmit } = useForm();
 
+  // const handleForm = (data, e) => {
+  // 	console.log('datos enviados')
+  // 	e.target.reset();
+  // 	swal('Su consulta ha sido enviada', 
+  //     {
+  // 		icon: "success",
+  // 	});
+  // };
   
-    return (
-        <div classsName='div-map'>
-            <div classsName='div-image-map'>
-				<LoadScript googleMapsApiKey='AIzaSyCS4pgjuCkMa8WJMa7fx9qQBicC9BhdTrg' className='loadScript-div'>
-					<GoogleMap
-						mapContainerStyle={mapStyle}
-						zoom={15}
-						center={location}
-					>
-						<Marker position={location}></Marker>
-					</GoogleMap>
-				</LoadScript>
-                </div>
-                <div className='location-info'>
-				<h2> Nos encontramos en Cordoba </h2>
-                <h3>Por cualquier consulta escribinos:</h3>
-                <p>Por Whatsapp: </p>
-                <a href={WHATSAPP_LINK} target="_blank" rel='noopener noreferrer'><FaWhatsapp /></a>
+  return (
+  <div classsName='div-map'>
+    <div classsName='div-image-map'>
+      <LoadScript googleMapsApiKey={GOOGLE_API_KEY} className='loadScript-div'>
+        <GoogleMap
+          mapContainerStyle={mapStyle}
+          zoom={15}
+          center={location}
+        >
+          <Marker position={location}></Marker>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+      <div className='location-info'>
+        <h2> Nos encontramos en Cordoba </h2>
+        <h3>Por cualquier consulta escribinos:</h3>
+        <p>Por Whatsapp: </p>
+        <a href={WHATSAPP_LINK} target="_blank" rel='noopener noreferrer'><FaWhatsapp /></a>
 
-                
-                <div>
-				<p>Por mail:</p>
-				{/* <form onSubmit={handleSubmit(handleForm)}>
-					<h3>Nombre y apellido </h3>
-					<input
-						className='inputs'
-						type='text'
-						{...register('name', { required: true, minLength: 4 })}
-					/>
-					<span>{errors?.name?.message}</span>
-					<h3>Asunto: </h3>
-					<input
-						className='inputs'
-						type='text'
-						{...register('subject', { required: true, minLength: 3 })}
-					/>
-					<h3>Mensaje: </h3>
-					<textarea
-						className='messageInput'
-						{...register('message', {
-							required: true,
-							minLength: 4,
-							maxLength: 150,
-						})}
-					></textarea>
-					<br />
-					<button className='contactBtn'>enviar</button>
-				</form> */}
-			</div>
-        </div>
-	</div>
-    )
+          <div>
+            <p>Por mail:</p>
+            {/* <form onSubmit={handleSubmit(handleForm)}>
+            <h3>Nombre y apellido </h3>
+            input
+            className='inputs'
+            type='text'
+            {...register('name', { required: true, minLength: 4 })}
+            />
+
+            <span>{errors?.name?.message}</span>
+            <h3>Asunto: </h3>
+            <input
+            className='inputs'
+            type='text'
+            {...register('subject', { required: true, minLength: 3 })}
+            />
+
+            <h3>Mensaje: </h3>
+				<textarea
+				className='messageInput'
+                {...register('message', {
+                required: true,
+                minLength: 4,
+                maxLength: 150,
+                })}
+                ></textarea>
+                <br />
+                <button className='contactBtn'>enviar</button>
+          </form> */}
+          </div>
+      </div>
+    </div>
+  )
 }
-
-export default FindUs;
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,8 +1,10 @@
 const numChicha = '5493515992780';
 const mensajeChicha ='Che quiero comprar birras🍻';
-export const WHATSAPP_LINK = `https://wa.me/${numChicha}?text=${encodeURIComponent(mensajeChicha)}`;
 const USER = 'USER';
 const ADMIN = 'ADMIN';
+
+export const WHATSAPP_LINK = `https://wa.me/${numChicha}?text=${encodeURIComponent(mensajeChicha)}`;
+export const GOOGLE_API_KEY = 'AIzaSyC7MPsGJ-Zbo5Tdt4xof6TPywH970pO0zw';
 export const ROLE = {
   USER,
   ADMIN
