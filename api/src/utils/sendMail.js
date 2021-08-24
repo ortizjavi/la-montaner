baa14741cddb3b14261527508b57ea8429389bba
baltasar <baltaguirre@gmail.com>
2021-08-24 10:43:09 -0300

    fix(/api/src/utils/sendMail.js): fix error in variable payment line 57

--- api/src/utils/sendMail.js
+++ api/src/utils/sendMail.js
@@ -54,7 +54,7 @@ sendEmail.processingOrder = (email, name, payment, shipping) => {
 	} else {
 		paymentTitle = 'Pagaste';
 	}
-	data = data.replace('{paymentTitle}', `${payme} $ ${payment.total}`);
+	data = data.replace('{paymentTitle}', `${payment} $ ${payment.total}`);
 	let shippingTitle, shippingAddress;
 	if(shipping.delivery){
 		shippingTitle = 'Envío a domicilio';
