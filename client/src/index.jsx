a640f54c3edda2cf65bf21e92df8560d664e3d89
ortizjavi <ortizjavier160994@gmail.com>
2021-09-04 12:25:44 -0300

    useless change to redeploy vercel

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
